cb7c96cb16cb6728faa3a6e202477ee87e2a61c1
Nikhil Marathe <nsm.nikhil@gmail.com>
2016-09-25 11:48:31 -0700

    Fix gethostname memory issues.
    Thanks to bluss on #rust.
    Turns out from_vec_unchecked() first pushes, then calls box slice. The
    latter leads to the vector being resized! This means we end up with
    a capacity 1 vector backing operations, and then we start overwriting
    memory.
    
    Even safer, but slightly slower (not that it matters) usage
    - https://is.gd/SYTni3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate libc;
+extern crate memchr;
 
 use libc::clone;
 use std::ffi::CString;
@@ -11,34 +12,51 @@ fn perror(a: &str) {
     }
 }
 
+fn sethostname(new: &str) {
+    let cs = CString::new(new).unwrap();
+    let ptr = cs.as_ptr();
+    let r = unsafe { libc::sethostname(ptr, new.len()) };
+    if r == -1 {
+        perror("sethostname");
+    }
+}
+
 fn gethostname() -> String {
-    let mut hostname = Vec::with_capacity(256);
-    let cs = unsafe { CString::from_vec_unchecked(hostname) };
-    let ptr = cs.into_raw();
-    let r = unsafe { libc::gethostname(ptr, 256) };
+    let mut hostname: Vec<u8> = Vec::with_capacity(255);
+    let ptr = hostname.as_mut_ptr();
+    let r = unsafe { libc::gethostname(ptr as *mut libc::c_char, 255) };
     if r == -1 {
         perror("gethostname");
+        return String::new();
     }
 
-    // retake ownership
-    let own = unsafe { CString::from_raw(ptr) };
-
-    let s = own.into_string().unwrap();
+    unsafe {
+        let len = CStr::from_ptr(ptr as *const libc::c_char).to_bytes().len();
+        hostname.set_len(len);
+    }
+    let s = String::from_utf8(hostname).unwrap();
     return s;
 }
 
 extern "C" fn enter(_: *mut libc::c_void) -> libc::c_int {
     let mypid = unsafe { libc::getpid() };
-    println!("I'm called from clone. Pid {}", mypid);
+    println!("Child pid {}", mypid);
+    let s = gethostname();
+    {
+        println!("Original host name in child {}", s);
+    }
+    sethostname("temporary-only-in-child");
     println!("Host name in child {}", gethostname());
 
-    std::thread::sleep(std::time::Duration::from_secs(30));
+    println!("Sleeping for 10 seconds");
+    std::thread::sleep(std::time::Duration::from_secs(10));
     return 0;
 }
 
 fn child_in_new_uts() {
     let mypid = unsafe { libc::getpid() };
-    println!("Original program Pid {}", mypid);
+    println!("Parent pid {}", mypid);
+    println!("Original host name in parent {}", gethostname());
 
     const STACK_SIZE: usize = 1024 * 1024;
     let mut stack = Vec::with_capacity(STACK_SIZE);
@@ -55,15 +73,15 @@ fn child_in_new_uts() {
     if child_pid == -1 {
         perror("clone");
     }
-    println!("Clone ret {}", child_pid);
 
     let wait_r = unsafe { libc::waitpid(child_pid, std::ptr::null_mut(), 0) };
     if wait_r == -1 {
         perror("waitpid");
     }
+
+    println!("Host name in parent {}", gethostname());
 }
 
 fn main() {
-    println!("Hello");
     child_in_new_uts();
 }
